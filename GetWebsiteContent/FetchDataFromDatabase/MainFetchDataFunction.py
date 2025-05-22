0e7efeebb790ee9658bd980200fb30326cd2b0d3
Mathies Klerck <mathiesklerck@MBP-von-Mathies.fritz.box>
2025-05-22 20:41:23 +0200

    update script for handling if no mail is found

--- GetWebsiteContent/FetchDataFromDatabase/MainFetchDataFunction.py
+++ GetWebsiteContent/FetchDataFromDatabase/MainFetchDataFunction.py
@@ -5,7 +5,7 @@ def fetch_data_from_database():
 
     supabase = create_supabase_client()
 
-    response = supabase.table('Locality').select("locality_id", "name", "email_from_osm", "website").is_("error_code", "null").is_("has_discounts", "null").is_("email_sent", "false").is_("no_email_avaiable", "null").not_.is_("website", "null").execute()
+    response = supabase.table('Locality').select("locality_id", "name", "email_from_osm", "website").is_("error_code", "null").is_("has_discounts", "null").is_("email_sent", "false").not_.is_("website", "null").execute()
     response_df = pd.DataFrame(response)
 
     fetched_localities = response_df[1][0] 
